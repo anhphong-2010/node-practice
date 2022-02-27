51173e563f381dc164785acc8699651035abcf0b
anhphong-2010 <anhphongnw.2010@gmail.com>
2022-02-27 13:17:32 +0700

    section on progress - 123 done

--- models/product.js
+++ models/product.js
@@ -17,14 +17,15 @@ const getProductFromFile = (callBack) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(title, imageUrl, price, description) {
     this.title = title;
     this.imageUrl = imageUrl;
-    this.description = description;
     this.price = price;
+    this.description = description;
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -35,5 +36,12 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductFromFile((products) => {
+      const product = products.find((b) => b.id === id);
+      cb(product);
+    });
   }
 };
--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,15 @@ const shopController = require("../controllers/shop");
 
 router.get("/", shopController.getIndex);
 
-router.get("/products", shopController.getProduct);
+router.get("/products", shopController.getProducts);
+
+router.get("/products/delete");
+
+router.get("/products/:productId", shopController.getProduct);
 
 router.get("/cart", shopController.getCart);
+
+router.post("/cart", shopController.postCart);
 
 router.get("/orders", shopController.getOrder);
 
